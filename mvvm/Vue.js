745127ac3eecee8956e3599e9ff7b08f4dea6225
benson_lybenson <benson_lybenson@git.cloud.tencent.com>
2018-09-18 18:07:13 +0800

    proxy data

--- mvvm/Vue.js
+++ mvvm/Vue.js
@@ -7,8 +7,17 @@ class Vue {
     if (this.$el) {
       // 数据劫持
       new Observer(this.$data)
+
+      // data代理到实例上
+      this.proxyData(this.$data)
+
       // 用数据和元素进行编译
       new Compile(this.$el, this)
     }
+  }
+  proxyData (data) {
+    Object.keys(data).forEach(key => {
+      this[key] = data[key]
+    })
   }
 }
